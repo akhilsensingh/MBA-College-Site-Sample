5f2a83ed825e9ae428aaf604a7238a0fca7298ef
akhilsensingh <a.sincyphers@gmail.com>
2025-03-19 23:05:01 +0530

    Acute issue resolve

--- src/app/main/components/switchSectionComponents/ContentSectionComponents/AdmissionsComponents/ASection4.js
+++ src/app/main/components/switchSectionComponents/ContentSectionComponents/AdmissionsComponents/ASection4.js
@@ -100,7 +100,7 @@ export default function ASection4() {
   }, []);
 
   return (
-    <div className="max-w-6xl mx-auto p-4 md:p-8">
+    <div className="max-w-6xl text-black mx-auto p-4 md:p-8">
       <div className="relative">
         {/* Vertical line */}
         <div className="absolute left-[28px] md:left-1/2 top-0 bottom-0 w-[2px] bg-gray-200 -ml-[1px]"></div>
